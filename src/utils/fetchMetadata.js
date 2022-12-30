5931cbcbc521aba2607505ba826b4b948307d99c
Leo Stelon <53221136+leostelon@users.noreply.github.com>
2022-12-30 14:47:57 +0530

    ERC1155Batch removed log queue

--- src/utils/fetchMetadata.js
+++ src/utils/fetchMetadata.js
@@ -1,6 +1,7 @@
 const { default: axios } = require("axios");
 const { getIpfsUrl } = require("./getIpfsUrl");
 
+// TODO: Add timeout of 10 seconds
 const fetchMetadata = async function (tokenURI, tokenId) {
 	try {
 		if (!tokenURI || tokenURI === "") return;
